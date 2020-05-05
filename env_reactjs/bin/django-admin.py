e78137d74bc108311e89b41a8fe84c6a626cc507
huynhdh <huynhdh@minerva.vn>
2020-05-05 15:26:40 +0700

    afd

---
+++ env_reactjs/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minerva/Documents/freeReactjs/django-reactjs-jwt/env_reactjs/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
